8ff560517654b03b46c6de1ddc82ede56c1bb479
oguzmydnc-dev <oguzmeydanci@gmail.com>
2025-12-05 23:57:24 +0300

    GPT

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,59 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const NotDefteriApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class NotDefteriApp extends StatelessWidget {
+  const NotDefteriApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Not Defteri',
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+        brightness: Brightness.light,
+      ),
+      home: const NotListesiSayfasi(),
+    );
+  }
+}
+
+class NotListesiSayfasi extends StatefulWidget {
+  const NotListesiSayfasi({super.key});
+
+  @override
+  State<NotListesiSayfasi> createState() => _NotListesiSayfasiState();
+}
+
+class _NotListesiSayfasiState extends State<NotListesiSayfasi> {
+  List<String> notlar = []; // Basit bir not listesi
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Not Defteri"),
+      ),
+      body: ListView.builder(
+        itemCount: notlar.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(notlar[index]),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _yeniNotEkle,
+        child: const Icon(Icons.add),
       ),
     );
+  }
+
+  void _yeniNotEkle() {
+    setState(() {
+      notlar.add("Yeni Not ${notlar.length + 1}");
+    });
   }
 }
